fd9181fac743e43bfe17916c99ee651a9936fd2a
#PRERNA CHIKERSAL# <PRERNA1@e.ntu.edu.sg>
2014-11-07 17:42:00 +0800

    unigram unk new

--- code/UnigramSVMIgnoreUNK.py
+++ code/UnigramSVMIgnoreUNK.py
@@ -6,7 +6,7 @@ import extractTypes #importing file for extracting types from input
 from collections import OrderedDict # OrderedDict to sort feature names in dictionary struct
 
 #filename = "test_input.tsv";
-filename = "../rawdata/train/twitter-train-cleansed-B_rmnotav.tsv";
+filename = "../rawdata/train/twitter-train-cleansed-B_rmnotav_ADDEDtest.tsv";
 startColIdx = 3;
 
 typesDict = extractTypes.extractTypes(filename, startColIdx);
@@ -31,37 +31,45 @@ print "Class Label Vector Y Extraction Started";
 YLabels = handleClassLabels.extractClassLabels(filename, labelIdx);
 print "Class Label Vector Y of size ", len(YLabels), " extracted";
 
+#Setting up scaler for standardisation
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler();
+
 
 # Training SVM
 from sklearn import svm
-from sklearn import linear_model
 print "Declaring SVM"
-#clf = svm.LinearSVC(); # linearsvc1
-clf = svm.LinearSVC(class_weight='auto'); # linearsvc2
+clf = svm.LinearSVC(C=0.01, class_weight='auto', penalty='l1', dual=0); # linearsvc2
 #clf = svm.SVC(cache_size = 1000, class_weight='auto', kernel = 'poly'); # Predicts all as POSITIVE :((
 #clf = linear_model.SGDClassifier();  # not tried yet
+print "standardising training data"
+Xfeatures = scaler.fit_transform(Xfeatures, YLabels);
 print "Fitting Data To SVM"
 clf.fit(Xfeatures, YLabels);
 print "SVM trained"
 
 
 # Saving Trained Classifier
-from sklearn.externals import joblib
-print "Saving SVM"
-fileToSave = "UnigramSVMClassifier.joblib.pkl";
-_ = joblib.dump(clf, fileToSave, compress=9);
-print "Classifier SAVED!";
+#from sklearn.externals import joblib
+#print "Saving SVM"
+#fileToSave = "UnigramSVMClassifier.joblib.pkl";
+#_ = joblib.dump(clf, fileToSave, compress=9);
+#print "Classifier SAVED!";
 
 
 
 #### CAN BE INEFFICIENT! CAN MAKE PREDICTIONS LINE BY LINE, IF WE FACE ISSUES
 # Extract feature vector from test data
-testFilename = "../rawdata/test/input/twitter-dev-input-B_rmnotav.tsv"
+#testFilename = "../rawdata/test/input/twitter-dev-input-B_rmnotav.tsv"
+testFilename = "../rawdata/test/AB_SemEval2013_task2_test_fixed/input/sms-test-input-B.tsv"
 testStartColIdx = 3
 print "XTEST Feature Vector Extraction Started";
 XTestFeatures = extractFeatureVecX.extractFeatureVecX(testFilename, testStartColIdx, typesDict);
 print "XTEST Feature Vector of size ", len(XTestFeatures), " extracted";
+print "standardising test data"
+XTestFeatures = scaler.transform(XTestFeatures);
 #Using Trained SVM to classify data
+print "Predicting labels for test data"
 predictedLabels = clf.predict(XTestFeatures);
 
 #Writing predicted labels to file
